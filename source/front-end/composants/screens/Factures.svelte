50f259efe1cf4bfccbdeb4b8bd20f9589b2c079f
David Bruant <davidbruant@protonmail.com>
2023-05-06 11:12:08 +0200

    Rajout de feedback à la soumission du formulaire + reset quand l'envoi s'est bien passé

--- source/front-end/composants/screens/Factures.svelte
+++ source/front-end/composants/screens/Factures.svelte
@@ -2,6 +2,7 @@
     //@ts-check
     
     import Skeleton from '../Skeleton.svelte'
+    import Loader from '../Loader.svelte'
 
     import '../../../format-données/types.js'
 
@@ -37,56 +38,80 @@
     let montantTVA
     let compteProduit
 
+    let factureSent = undefined;
+
     function créerFacture(e){
-        créerEnvoiFactureÀClient({compteClient, identifiantFacture, dateFacture, montantHT, montantTVA, compteProduit})
+        factureSent = créerEnvoiFactureÀClient({compteClient, identifiantFacture, dateFacture, montantHT, montantTVA, compteProduit})
+
+        factureSent.then(() => {
+            factureSent = undefined
+            e.target.reset()
+        })
+    }
+
+    function isPromise(x){
+        return x === Object(x) && typeof x.then === 'function'
     }
 
 </script>
 
 <Skeleton {login} {logout}>
     <h1>Voici les factures de l'organisation <code>{org}</code></h1>
-    
+
     <h2>Liste des factures</h2>
     <p>à faire ...</p>
 
     <h2>Saisir les données d'une facture</h2>
 
     <form on:submit|preventDefault={créerFacture}>
+        <fieldset disabled={isPromise(factureSent)}>
+            <label>
+                <div>Client</div>
+                <input bind:value={compteClient} placeholder="411xxxx">
+            </label>
+            <label>
+                <div>Identifiant de facture</div>
+                <input bind:value={identifiantFacture} type="text">
+            </label>
+            <label>
+                <div>Date</div>
+                <input bind:value={dateFacture} type="date">
+            </label>
+            <label>
+                <div>Montant HT (€)</div>
+                <input bind:value={montantHT} step="0.01" type="number">
+            </label>
+            <label>
+                <div>Montant TVA (€)</div>
+                <input bind:value={montantTVA} step="0.01" type="number">
+            </label>
+            <label>
+                <div>Compte Produit</div>
+                <input bind:value={compteProduit} placeholder="706xxx">
+            </label>
 
-        <label>
-            <div>Client</div>
-            <input bind:value={compteClient} placeholder="411xxxx">
-        </label>
-        <label>
-            <div>Identifiant de facture</div>
-            <input bind:value={identifiantFacture} type="text">
-        </label>
-        <label>
-            <div>Date</div>
-            <input bind:value={dateFacture} type="date">
-        </label>
-        <label>
-            <div>Montant HT (€)</div>
-            <input bind:value={montantHT} step="0.01" type="number">
-        </label>
-        <label>
-            <div>Montant TVA (€)</div>
-            <input bind:value={montantTVA} step="0.01" type="number">
-        </label>
-        <label>
-            <div>Compte Produit</div>
-            <input bind:value={compteProduit} placeholder="706xxx">
-        </label>
-
-        <button type="submit">Créer la facture</button>
-
+            <div class="button-with-loader">
+                <button type="submit">Créer la facture</button>
+                {#await factureSent}
+                    <Loader></Loader>
+                {:catch err}
+                    Problème avec l'envoi de la facture {err}
+                {/await}
+            </div>
+        </fieldset>
     </form>
 </Skeleton>
 
 <style lang="scss">
-    form label{
-        input{
+    form {
+        label input{
             margin-left: 1rem;
+        }
+
+        .button-with-loader{
+            display: flex;
+            flex-direction: row;
+            align-items: flex-start;
         }
     }
 </style>
