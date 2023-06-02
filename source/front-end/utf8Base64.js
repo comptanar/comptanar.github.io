d75ab025283b4c45eba0347ebbffd68f1f09282d
David Bruant <davidbruant@protonmail.com>
2023-06-02 16:32:57 +0200

    Tentative de résolution des histoires d'encodage

---
+++ source/front-end/utf8Base64.js
@@ -0,0 +1,20 @@
+//@ts-check
+
+// adapted from https://developer.mozilla.org/en-US/docs/Glossary/Base64#solution_1_%E2%80%93_escaping_the_string_before_encoding_it
+/**
+ * 
+ * @param {string} s // cleartext string
+ * @returns {string} // utf-8-encoded base64 string
+ */
+export function UTF8ToB64(s) {
+    return btoa(unescape(encodeURIComponent(s)));
+}
+
+/**
+ * 
+ * @param {string} s // utf-8-encoded base64 string
+ * @returns {string} // cleartext string
+ */
+export function b64ToUTF8(s) {
+    return decodeURIComponent(escape(atob(s)));
+}
