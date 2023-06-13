cc640836c5166325558b2624141b289244bade92
Ana Gelez <ana@gelez.xyz>
2023-06-13 16:47:06 +0200

    Petits changements dans le CSS

--- source/front-end/stringifiers.js
+++ source/front-end/stringifiers.js
@@ -4,20 +4,12 @@ import { sum } from 'd3-array'
 import { differenceInDays, differenceInMonths, formatDistanceToNow, format } from 'date-fns'
 import { fr } from 'date-fns/locale'
 
-export function displayDate(date){
-    if(differenceInDays(date, new Date()) === 0){
+export function displayDate(date) {
+    if (differenceInDays(date, new Date()) === 0) {
         return `Aujourd'hui`
     }
 
-    if (differenceInDays(date, new Date()) > 0) {
-        return `dans ${formatDistanceToNow(date, {locale: fr})}`
-    }
-    
-    if(differenceInMonths(date, new Date()) > -3){
-        return `il y a ${formatDistanceToNow(date, {locale: fr})}`
-    }
-    
-    return format(date, 'd MMMM yyyy', {locale: fr})
+    return format(date, 'd MMMM yyyy', { locale: fr })
 }
 
 /** @type {(ops: OpérationDeCompte[]) => number} */
