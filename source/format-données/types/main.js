e4c704eda62b2cae3677c43f64a70cdcc3766829
David Bruant <davidbruant@protonmail.com>
2023-08-08 16:14:44 +0200

    Renomme BaseOpérationHautNiveau.identifiantOpération en identifiant par uniformité

--- source/format-données/types/main.js
+++ source/format-données/types/main.js
@@ -11,7 +11,7 @@
 
 /**
  * @typedef {Object} BaseOpérationHautNiveau
- * @property {string} identifiantOpération
+ * @property {string} identifiant
  * @property {string} type
  * @property {Date} date
  * @property {OpérationDeCompte[]} [opérations]
